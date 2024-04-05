f649fbf7a88fdb2cd7d40c7093f83f527d91ef68
chenkexin <chenkx1n@163.com>
2024-04-05 23:27:48 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css'
+
+import Game from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Game />
+);
+
